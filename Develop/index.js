5c8efe7c29e0e121be8da1b969519dbac1918d5a
I am Billie <dosbor3@gmail.com>
2022-01-25 22:32:14 -0600

    added project questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,67 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter your project's title: ",
+        validate: title => {
+            if (title) {
+              return true;
+            }
+            else {
+              console.log("Please enter your project's title!");
+              return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide: ", 
+    },
+    {
+        type: "input", 
+        name: "installation",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. "
+    },
+    {
+        type: "input", 
+        name: "usage", 
+        message: "Provide instructions and examples for use. Include screenshots as needed."
+
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select all licensing that apply below: ",
+        choices: ["The MIT License", "Mozilla Public License 2.0", "Attribution License (BY)", "Open Database License (ODbL)", 
+        "Public Domain Dedication and License (PDDL)", "Artistic-2.0", "Open Font-1.1" ]
+    },
+    {
+        type: "confirm",
+        name: "confirmContributions", 
+        message: "Do you wish to add any contributions? ",
+        default: true
+    },
+    {
+        type: "input", 
+        name: "contributions",
+        message: "List your contributor's FULL NAME followed by a [SPACE] and then their github account: ",
+        when: ({confirmContributions}) => confirmContributions
+    },
+    {
+        type: "input", 
+        name: "tests",
+        message: "Include your test and instructions on how to run them here: "
+    }, 
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
