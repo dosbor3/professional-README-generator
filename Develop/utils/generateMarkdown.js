f6bf8c79d34ae3900e0ca2c1901c3395efee4677
I am Billie <dosbor3@gmail.com>
2022-01-30 17:30:03 -0600

    added images of application

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -63,7 +63,17 @@ ${projectsArr.tests}
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.  Source code can be found at http://github.com/${projectsArr.github}.  Should
-you have any questions, comments, or concerns, please feel free to reach out via email at ${projectsArr.email}.  
+you have any questions, comments, or concerns, please feel free to reach out via email at ${projectsArr.email}. 
+
+## Image of Application 
+![applicationImage](https://user-images.githubusercontent.com/40706088/151722257-a445292a-e961-41cf-9243-081c3a5c8cc4.jpg)
+
+
+
+## Image 2 of Application
+![applicationImage2](https://user-images.githubusercontent.com/40706088/151722352-280586a1-a854-4a53-8cdc-596bd19c0aec.jpg)
+
+
 `
 }
 
